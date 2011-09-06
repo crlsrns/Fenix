6ae05bf9f25e566cc9bc5b0fe8bc8f3b6ac2078b
Carlos Arenas <carlos.arenas@gmail.com>
2011-09-05 23:35:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fenix::Application.config.secret_token = 'a27e43a95121c3d44bff31405f18b8f9ca25f31666c85e135fdbf0b80d8867fd808600e6c0be4912cbb910a5bf3b3e1426b8c9ccbd0d437b0c2ba36bc30035e3'
